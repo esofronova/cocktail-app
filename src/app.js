692ba0063e7c5e2b1c83a4bbd4bd69e835881535
esofronova <essofronova@gmail.com>
2021-11-02 16:10:05 -0700

    small fixes

--- src/app.js
+++ src/app.js
@@ -25,18 +25,18 @@ export default function CocktailApp() {
   return (
     <div className="cocktail-app bg-cover py-sm-3 h-100">
       <div className="container h-100">
-        <div className="border rounded p-3 text-white cocktail-form h-100">
-          <div className="d-flex justify-content-between align-items-center" style={{ height: "10%" }}>
-            <h3 className="fw-bold ms-3 text-white mix-drink">Mix Me a Drink!</h3>
+        <div className="p-3 text-white cocktail-form h-100">
+          <div className="d-flex justify-content-between align-items-center" style={{ height: "7%" }}>
+            <h3 className="fw-bold ms-3 text-white mix-drink mb-0">Mix Me a Drink!</h3>
             {cocktail !== "All" ?
               <button
-                className="btn text-white shadow-none fs-3"
+                className="btn text-white shadow-none fs-3 p-0 d-flex"
                 onClick={() => { setCocktail("All") }}
               ><ArrowReturnLeft /></button>
               :
               ""}
           </div>
-          <div className="d-flex flex-wrap" style={{ height: "90%" }}>
+          <div className="d-flex flex-wrap" style={{ height: "93%" }}>
             <Leftbar />
             {cocktail === "All" ? <AllCocktails /> : <Cocktail />}
           </div>
--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -4,16 +4,18 @@ import { cocktail } from "../App";
 export default function Cocktail() {
 
   return (
-    <div className="col-12 col-md-8 p-3 mt-3 mt-md-0 h-50 h-md-100 overflow-auto">
+    <div className="col-12 col-md-8 mt-3 mt-md-0 h-50 h-md-100 overflow-auto">
       {cocktails.map((item, index) => {
         return (
-          <div key={index} className={"mx-3 mx-md-0" + item.name + (item.name === cocktail ? "" : " d-none")}>
+          <div key={index} className={"mx-3 mx-md-0 " + item.name + (item.name === cocktail ? "" : " d-none")}>
+            <div>
+              <h3 className="fw-bold text-center mb-3">{item.name}</h3>
+            </div>
             <div className="row mb-3">
               <div className="col-7 col-sm-6 col-lg-4 mb-3">
                 <img src={item.img} className="w-100 h-100 of-cover" alt={item.name} />
               </div>
               <div className="col-5 col-sm-6 col-lg-8">
-                <h3 className="fw-bold">{item.name}</h3>
                 <h5>Ingredients</h5>
                 {item.ingredients.map((item, index) => {
                   return (
